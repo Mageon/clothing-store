6492e895bee02854c1be56e42979f57d6ff95ad1
Mageon <axelord@gmail.com>
2021-05-15 10:43:27 -0500

    Adding header component and links to home, shop, page and contact

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import HomePage from './pages/homepage/hompage.component';
 import { Route, Switch } from 'react-router-dom';
 import { Turorial } from './pages/tuto/tuto';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      {/* <HomePage/> */}
-      <Switch>
-        <Route exact path='/' component={HomePage}/>        
-        <Route exact path='/tuto' component={Turorial}/>
-        <Route exact path='/shop' component={ShopPage}/>
-
-      </Switch>
-      {/* Swithch renders the first match of the Route component, then renders nothing else */}
+      <div>
+        <Route path='/' component={Header}/>
+      </div>
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>        
+          <Route exact path='/tuto' component={Turorial}/>
+          <Route exact path='/shop' component={ShopPage}/>
+        </Switch>
+        {/* Switch renders the first match of the Route component, then renders nothing else */}
+      </div>
     </div>
   );
 }
--- src/pages/homepage/hompage.component.jsx
+++ src/pages/homepage/hompage.component.jsx
@@ -3,11 +3,9 @@ import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 // import { Link } from 'react-router-dom';
 
-
 const HomePage = (props) => {
-  // console.log(props);
   return (    
-    <div className="HomePage">
+    <div className="homepage">
       {/* <Link to='/hats'>Hats Page</Link>
           // This one is like a tag but it rendes in our app the link we give it 
           // Now we can use the props to make a button to link to that
